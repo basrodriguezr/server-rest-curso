16dfb09fd2802469c4d1380f94b204e3e79165a5
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-20 18:49:54 -0400

    google sign in y sign out

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const {response} = require('express');
+const {response, json} = require('express');
 const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const{ generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req,res = response)=>{
     const {correo, password} = req.body;
@@ -42,6 +43,49 @@ const login = async (req,res = response)=>{
     }
 }
 
+const googleSignIn = async (req,res = response)=>{
+    const { id_token } = req.body;
+
+    try {
+        const {nombre,img,correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            usuario = new Usuario({
+                nombre,
+                img,
+                correo,
+                google:true,
+                password:':p',
+                role:'USER_ROLE'
+            });
+            
+            await usuario.save();
+        }
+
+        if(!usuario.estado){
+            return res.status(400).json({
+                msg: 'Usuario inactivo'
+            });
+        }
+
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no es valido'
+        });
+    }
+
+}
+
 module.exports={
-    login
+    login,
+    googleSignIn
 }
